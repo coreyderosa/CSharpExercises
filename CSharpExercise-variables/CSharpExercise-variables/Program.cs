be27d05b6343ff5561799fd427a90ae6be045bdb
coreyderosa <coreyderosa@gmail.com>
2016-12-07 20:49:52 -0700

    Variables

---
+++ CSharpExercise-variables/CSharpExercise-variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercise_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            Console.WriteLine("You are {0} years old.", age);
+            Console.ReadKey();
+        }
+    }
+}
