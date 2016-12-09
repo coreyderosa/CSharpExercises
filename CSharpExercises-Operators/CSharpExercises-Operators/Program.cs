379efce73211fe84850f79453f026375d80688b8
coreyderosa <coreyderosa@gmail.com>
2016-12-08 18:24:09 -0700

    Operators- grid output

---
+++ CSharpExercises-Operators/CSharpExercises-Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises_Operators
+{
+    class Program
+    {
+        static void output()
+        {
+            int x = 10;
+            int y = 5;
+
+            Console.WriteLine("x value\t\ty value\t\tExpressions\t\tResult");
+
+            Console.WriteLine("{0,-8}\t{1,-8}\tx=y+3\t\t\tx={2,-8}", x, y, y + 3);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx=y-2\t\t\tx={2,-8}", x, y, y - 2);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx=y*5\t\t\tx={2,-8}", x, y, y * 5);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx=x/y\t\t\tx={2,-8}", x, y, (float)x / y);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx=x%y\t\t\tx={2,-8}", x, y, x % y);
+
+
+            Console.Read();
+
+        }
+
+        static void Main(string[] args)
+        {
+            output();
+        }
+    }
+}
+        
+    
+
