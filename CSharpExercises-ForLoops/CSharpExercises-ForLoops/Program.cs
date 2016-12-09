c6fbc32b57848cc2f13105d05ed40029c18bad64
coreyderosa <coreyderosa@gmail.com>
2016-12-08 21:15:04 -0700

    For Loops- exercise 1

---
+++ CSharpExercises-ForLoops/CSharpExercises-ForLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises_ForLoops
+{
+    class Program
+    {
+        static void exercise1()
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                for (int j = 1; j <= 7-i; j++)
+                Console.WriteLine("*");
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            exercise1();
+        }
+    }
+}
