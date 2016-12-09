2c07615a09430f50fd9275decb6522c3441e8bae
coreyderosa <coreyderosa@gmail.com>
2016-12-08 18:28:27 -0700

    Output 2

--- CSharpExercises-Operators/CSharpExercises-Operators/Program.cs
+++ CSharpExercises-Operators/CSharpExercises-Operators/Program.cs
@@ -8,6 +8,24 @@ namespace CSharpExercises_Operators
 {
     class Program
     {
+        static void output2()
+        {
+            int x = 10;
+            int y = 5;
+
+            Console.WriteLine("x value\t\ty value\t\tExpressions\t\tResult");
+
+            Console.WriteLine("{0,-8}\t{1,-8}\tx+=y\t\t\tx={2,-8}", x, y, x + y);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx-=y-2\t\t\tx={2,-8}", x, y, x - y + 2);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx*=y*5\t\t\tx={2,-8}", x, y, x * y * 5);
+            Console.WriteLine("{0,-8}\t{1,-8}\tx/=x/y\t\t\tx={2,-8}", x, y, (float)x / (x / y));
+            Console.WriteLine("{0,-8}\t{1,-8}\tx%y\t\t\tx={2,-8}", x, y, x % y);
+
+
+            Console.Read();
+
+        }
+
         static void output()
         {
             int x = 10;
@@ -28,7 +46,8 @@ namespace CSharpExercises_Operators
 
         static void Main(string[] args)
         {
-            output();
+            //output();
+            output2();
         }
     }
 }
