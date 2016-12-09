f48ae57df7b921be4a849ae44b0f499fd375feeb
coreyderosa <coreyderosa@gmail.com>
2016-12-08 20:44:50 -0700

    ConditionalStmt- Switch Case 2

--- CSharpExercises-ConditionalStmts2/CSharpExercises-ConditionalStmts2/Program.cs
+++ CSharpExercises-ConditionalStmts2/CSharpExercises-ConditionalStmts2/Program.cs
@@ -8,6 +8,41 @@ namespace CSharpExercises_ConditionalStmts2
 {
     class Program
     {
+
+        static void exercise2()
+        {
+            char PEOTUS;
+            Console.WriteLine("Who is the current President-Elect of the United States of America? \nA)Barack Obama \nB)Hilary Clinton \nC)Donald Trump");
+            PEOTUS = Convert.ToChar(Console.ReadLine());
+            switch (PEOTUS)
+            {
+                case 'A':
+                    Console.WriteLine("Wrong answer!");
+                    break;
+                case 'a':
+                    Console.WriteLine("Wrong answer!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Wrong answer!");
+                    break;
+                case 'b':
+                    Console.WriteLine("Wrong answer!");
+                    break;
+                case 'C':
+                    Console.WriteLine("You are correct!");
+                    break;
+                case 'c':
+                    Console.WriteLine("You are correct!");
+                    break;
+                default:
+                    Console.WriteLine("Choose A, B or C");
+                    break;
+            }
+
+            Console.Read();
+
+        }
+
         static void exercise1()
         {
             char numberKey;
@@ -15,7 +50,8 @@ namespace CSharpExercises_ConditionalStmts2
             numberKey = Convert.ToChar(Console.ReadLine());
             switch (numberKey)
             {
-                case '0': Console.WriteLine("You entered 0");
+                case '0':
+                    Console.WriteLine("You entered 0");
                     break;
                 case '1':
                     Console.WriteLine("You entered 1");
@@ -47,16 +83,16 @@ namespace CSharpExercises_ConditionalStmts2
                 default:
                     Console.WriteLine("You did not enter a number!");
                     break;
-
-                    
             }
+
             Console.Read();
         }
 
 
         static void Main(string[] args)
         {
-            exercise1();
+            exercise2();
+            //exercise1();
         }
     }
 }
