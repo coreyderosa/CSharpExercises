d0a0b15009c0b79cae7814214a56b5c5eb66f0af
coreyderosa <coreyderosa@gmail.com>
2016-12-08 19:41:14 -0700

    ConditionalStmts- Exercise 2

--- CSharpExercises-ConditionalStmts/CSharpExercises-ConditionalStmts/Program.cs
+++ CSharpExercises-ConditionalStmts/CSharpExercises-ConditionalStmts/Program.cs
@@ -8,6 +8,41 @@ namespace CSharpExercises_ConditionalStmts
 {
     class Program
     {
+
+        static void exercise2()
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Please enter the quiz score: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the mid-term score: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the final score: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            float avg = ((a + b + c) / 3);
+
+            if (avg >= 90)
+            {
+                Console.WriteLine("Grade is an A");
+            }
+            else if ((avg >= 80) && (avg < 90))
+            {
+                Console.WriteLine("Grade is an B");
+            }
+            else if ((avg >= 70) && (avg < 80))
+            {
+                Console.WriteLine("Grade is an C");
+            }
+            else
+            {
+                Console.WriteLine("Grade is an F");
+            }
+
+            Console.Read();
+        }
+
         static void exercise1()
         {
 
@@ -41,7 +76,8 @@ namespace CSharpExercises_ConditionalStmts
         }
         static void Main(string[] args)
         {
-            exercise1();
+            exercise2();
+            //exercise1();
         }    
     }
 }
