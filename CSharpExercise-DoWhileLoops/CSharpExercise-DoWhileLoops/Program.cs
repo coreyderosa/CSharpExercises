38edc8fd6ecf2ff35ca6fa1d0ba8e09d4b32abd3
coreyderosa <coreyderosa@gmail.com>
2016-12-09 20:40:20 -0700

    Do while loop- exercise1

---
+++ CSharpExercise-DoWhileLoops/CSharpExercise-DoWhileLoops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercise_DoWhileLoops
+{
+    class Program
+    {
+        
+
+        static void exercise1()
+        {
+            string reset = "y";
+            Console.WriteLine("What is the command keyword to exit a loop in C#?");
+            Console.WriteLine("A) int");
+            Console.WriteLine("B) continue");
+            Console.WriteLine("C) break");
+            Console.WriteLine("D) exit");
+            Console.WriteLine("Enter your answer: ");
+            string answer = Console.ReadLine();
+
+
+                do
+                {
+                    Console.WriteLine("Enter you answer: ");
+                    answer = Console.ReadLine();
+
+                    if (answer == "C" || answer == "c")
+                    {
+                        Console.WriteLine("Correct!");
+                        reset = "y";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect!");
+                        Console.WriteLine("Try again? y/n");
+                        reset = Console.ReadLine();
+                    }
+
+                }
+                while (reset == "y");
+                {
+
+                }
+            }
+        
+
+        static void Main(string[] args)
+        {
+            exercise1();
+        }
+    }
+}
