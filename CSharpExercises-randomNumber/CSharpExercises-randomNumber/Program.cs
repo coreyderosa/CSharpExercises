dce986700dfb73ed3b4f05705c8168fab6d152d2
coreyderosa <coreyderosa@gmail.com>
2016-12-09 20:52:58 -0700

    Random Number
    guess a number between 1-6

---
+++ CSharpExercises-randomNumber/CSharpExercises-randomNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises_randomNumber
+{
+    class Program
+    {
+        static void exercise1()
+        {
+            Random umm = new Random();
+            int a = umm.Next(1, 7);
+            Console.WriteLine("Guess a number between 1 and 6!");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == a)
+            {
+                Console.WriteLine("Nice guess!!!");
+            }
+            else
+            {
+                Console.WriteLine("Nice try but no cigar!");
+                Console.WriteLine("The correct number is {0}. Better luck next time.", a);
+            }
+
+            Console.Read();
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            //exercise2();
+            exercise1();
+        }
+    }
+}
