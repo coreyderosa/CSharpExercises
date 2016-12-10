abcaf58884a3f073b94646b6167c7c303e8e36f7
coreyderosa <coreyderosa@gmail.com>
2016-12-10 13:39:19 -0700

    BubbleSortArray-exercise2
    if (bubble[j] < bubble[j + 1])- descending
    if (bubble[j] > bubble[j + 1])- ascending

--- CSharpExercises-BubbleSortArray/CSharpExercises-BubbleSortArray/Program.cs
+++ CSharpExercises-BubbleSortArray/CSharpExercises-BubbleSortArray/Program.cs
@@ -8,7 +8,28 @@ namespace CSharpExercises_BubbleSortArray
 {
     class Program
     {
+        static void exercise2()
+        {
+            int[] bubble = { 50, 100, 30, 80, 10, 60, 20, 70, 90, 40 };
+            int b = 0;
 
+            for (int i = 0; i < bubble.Length; i++)
+            {
+                for (int j = 0; j < bubble.Length - 1; j++)
+                {
+                    if (bubble[j] < bubble[j + 1])
+                    {
+                        b = bubble[j + 1];
+                        bubble[j + 1] = bubble[j];
+                        bubble[j] = b;
+                    }
+                }
+            }
+            for (int h = 0; h < bubble.Length; h++)
+                Console.WriteLine(bubble[h] + " ");
+
+            Console.ReadLine();
+        }
         static void exercise1()
         {
             int[] bubble = { 50, 100, 30, 80, 10, 60, 20, 70, 90, 40 };
@@ -38,8 +59,8 @@ namespace CSharpExercises_BubbleSortArray
 
         static void Main(string[] args)
         {
-            //exercise2();
-            exercise1();
+            exercise2();
+            //exercise1();
         }
     }
 }
