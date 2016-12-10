9d1b8a2fee169e6524d87f06ce8beff009ec5028
coreyderosa <coreyderosa@gmail.com>
2016-12-09 21:15:52 -0700

    Random Number- exercise 2
    looping the random number question

--- CSharpExercises-randomNumber/CSharpExercises-randomNumber/Program.cs
+++ CSharpExercises-randomNumber/CSharpExercises-randomNumber/Program.cs
@@ -8,6 +8,35 @@ namespace CSharpExercises_randomNumber
 {
     class Program
     {
+        static void exercise2()
+        {
+            string reset = "y";
+            Random umm = new Random();
+            int a = umm.Next(1, 7);
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 6!");
+                int answer = Convert.ToInt32(Console.ReadLine());
+                if (answer == a)
+                {
+                    Console.WriteLine("Nice guess!!!");
+                    reset = "n";
+                }
+                else
+                {
+                    Console.WriteLine("Nice try but no cigar!");
+                    Console.WriteLine("The correct number is {0}. Better luck next time.", a);
+                    Console.WriteLine("Try again? y/n");
+                    reset = Console.ReadLine();
+                }
+            }
+            while (reset == "y");
+            
+
+        }
+
+
         static void exercise1()
         {
             Random umm = new Random();
@@ -32,8 +61,8 @@ namespace CSharpExercises_randomNumber
 
         static void Main(string[] args)
         {
-            //exercise2();
-            exercise1();
+            exercise2();
+            //exercise1();
         }
     }
 }
