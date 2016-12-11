19e15a55b9027d56dc0e891df90a986de738e300
coreyderosa <coreyderosa@gmail.com>
2016-12-10 18:26:14 -0700

    Array table/matrix

---
+++ CSharpExercises-arrayTable/CSharpExercises-arrayTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises_arrayTable
+{
+    class Program
+    {
+
+        static void exercise1()
+        {
+            int start = 1;
+            int[,] array = new int[5, 5];
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write("{0}\t", start++);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }        
+
+
+    static void Main(string[] args)
+        {
+            //exercise2();
+            exercise1();
+        }
+    }
+}
